8f03f0f6334c40b7e13a8681da7d8b4ed156f04c
lucaspires-source <pires.lucas94@gmail.com>
2021-06-16 09:50:13 -0300

    create and config knexfile.js

---
+++ knexfile.js
@@ -0,0 +1,16 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: "pg",
+    connection: {
+      database: "knex_test",
+      username: "postgres",
+      password: "postgres"
+    }
+  },
+
+
+
+};
