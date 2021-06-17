04ce00e14b4d7970bf6d47ba8bc8c2c192375d23
lucaspires-source <pires.lucas94@gmail.com>
2021-06-17 16:39:55 -0300

    worked on delete on usercontroller

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -25,5 +25,17 @@ module.exports = {
        }catch(error){
            next(error)
        }
+   },
+   async delete(req, res, next){
+       try {
+           const { id } = req.params
+           await knex('users')
+           .where({id})
+           .del()
+           return res.send
+       }
+       catch(error){
+           next(error)
+       }
    }
 }
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,9 @@
-const express = require('express');
-const routes = express.Router()
-const UserController = require('./controllers/UserController')
+const express = require("express");
+const routes = express.Router();
+const UserController = require("./controllers/UserController");
 
-routes.get('/users', UserController.index )
-routes.post('/users', UserController.create )
-routes.put('/users/:id', UserController.update )
-module.exports = routes
+routes.get("/users", UserController.index);
+routes.post("/users", UserController.create);
+routes.put("/users/:id", UserController.update);
+routes.delete("/users/:id", UserController.delete);
+module.exports = routes;
