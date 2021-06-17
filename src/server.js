e268efe10a9c5c6874ff9984a6ac3564b62f537d
lucaspires-source <pires.lucas94@gmail.com>
2021-06-17 14:33:46 -0300

    get route is done

--- src/server.js
+++ src/server.js
@@ -1,6 +1,13 @@
 const express = require('express')
+const knex = require('./database')
 const app = express()
 
 app.listen(3333,()=>{
     console.log('server is running on http:localhost://3333')
 })
+
+app.get('/users', (req, res) =>
+    knex('users').then((results) =>
+      res.json(results)
+    )
+)
