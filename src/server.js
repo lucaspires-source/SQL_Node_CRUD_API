63f720882847277f7beca8ba99200a33d43de840
lucaspires-source <pires.lucas94@gmail.com>
2021-06-17 16:59:30 -0300

    new migrations

--- src/server.js
+++ src/server.js
@@ -4,6 +4,13 @@ const app = express()
 app.use(express.json())
 app.use(routes)
 
+
+//notfound
+app.use((req, res, next) => {
+    const error = new Error('Not found')
+    error.status = 404
+    next(error)
+})
 //catch all 
 
 app.use((error, req, res,next) =>{
