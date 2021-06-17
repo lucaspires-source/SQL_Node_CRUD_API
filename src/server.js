c42380f30b85938947c342117f4b5671609dcb88
lucaspires-source <pires.lucas94@gmail.com>
2021-06-17 14:44:42 -0300

    code refactoring

--- src/server.js
+++ src/server.js
@@ -1,13 +1,8 @@
 const express = require('express')
-const knex = require('./database')
+const routes = require('./routes')
 const app = express()
-
+app.use(routes)
 app.listen(3333,()=>{
     console.log('server is running on http:localhost://3333')
 })
 
-app.get('/users', (req, res) =>
-    knex('users').then((results) =>
-      res.json(results)
-    )
-)
