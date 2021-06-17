7b8d7e0923457467baa3b72b5d56a09b65fb0166
lucaspires-source <pires.lucas94@gmail.com>
2021-06-17 16:25:24 -0300

    worked on create user controller

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -3,5 +3,22 @@ module.exports = {
     async index(req, res){
     const results = await knex('users')
     return res.json(results)
-}
+},
+    async create(req, res,next){
+        try {
+            const { username } = req.body
+            await knex('users').insert({username})
+            return res.status(201).send()
+        }catch(error){
+            next(error)
+        }
+   },
+
+   async update(req, res,next){
+       try {
+
+       }catch(error){
+           next(error)
+       }
+   }
 }
--- src/routes.js
+++ src/routes.js
@@ -3,5 +3,5 @@ const routes = express.Router()
 const UserController = require('./controllers/UserController')
 
 routes.get('/users', UserController.index )
-
+routes.post('/users', UserController.create )
 module.exports = routes
